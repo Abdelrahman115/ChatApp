b850291e5666fa9d72f3dac265dc69add3af67c2
Abdelrahman115 <abdelrahman.tharwat95@gmail.com>
2023-07-23 17:55:40 +0300

    Creating ViewControllers files / pod init

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("First Action")
-        print("Second Action")
     }
 
 
